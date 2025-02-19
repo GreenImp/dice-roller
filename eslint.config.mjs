c50b48062c706c98c975e13f83f1a736f76bad01
Lee Langley <lee@greenimp.co.uk>
2025-02-19 22:08:44 +0000

    Get linter working

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,15 +4,43 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
+  {
+    ignores: [
+      'coverage/**/*',
+      'docs/**/*',
+      'lib/**/*',
+      // 'types/**/*',
+      'src/parser/grammars/grammar.js',
+      // 'tests/**/*',
+    ],
+  },
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
   tseslint.configs.stylisticTypeChecked,
   {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
     rules: {
       // disable eslint rule so it doesn't conflict
-      "class-methods-use-this": "off",
-      "@typescript-eslint/class-methods-use-this": "error",
-      "@typescript-eslint/no-inferrable-types": "off"
-    }
-  }
+      'class-methods-use-this': 'off',
+      '@typescript-eslint/class-methods-use-this': 'error',
+      '@typescript-eslint/no-inferrable-types': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          "args": "all",
+          "argsIgnorePattern": "^_",
+          "caughtErrors": "all",
+          "caughtErrorsIgnorePattern": "^_",
+          "destructuredArrayIgnorePattern": "^_",
+          "varsIgnorePattern": "^_",
+          "ignoreRestSiblings": true,
+        },
+      ],
+    },
+  },
 );
